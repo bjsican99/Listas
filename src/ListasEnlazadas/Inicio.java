4424ea45695d55ce4d812074344146dcaeb6efb4
U. Mariano Gálvez <alumno@labs.academico>
2019-03-26 10:50:04 -0600

    Primera Version

---
+++ src/ListasEnlazadas/Inicio.java
@@ -0,0 +1,28 @@
+package ListasEnlazadas;
+
+/**
+ *
+ * @author EDUARDO
+ */
+public class Inicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListasEnlazadas miLista=new ListasEnlazadas();
+	miLista.InsertarEnPrimero(10);
+	miLista.InsertarEnPrimero(15);
+	miLista.InsertarEnPrimero(30);
+	miLista.InsertarEnPrimero(50);
+	miLista.InsertarEnPrimero(40);
+	miLista.InsertarEnPrimero(60);
+	System.out.println(miLista.Listar());
+	System.out.println("borrar");
+	miLista.borrarUltimo();
+        miLista.borrarPrimero();
+	
+	System.out.println(miLista.Listar());
+    }
+    
+}
